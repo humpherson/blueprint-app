3d5846bb3e8e0cc850c6c77878ee8d582bb7a43c
humpherson <dhumpherson@gmail.com>
2024-10-27 15:43:46 +0000

    Updating next.config.mjs basePath and assetProfix values

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "", // Remove any subdirectory, leave this empty
+  assetPrefix: "", // Ensure assets are loaded correctly from the root
+};
 
 export default nextConfig;
